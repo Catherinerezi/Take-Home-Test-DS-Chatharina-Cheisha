db86217fbe9566a146b44be62394a8874f704009
Chatharina Cheisha <catherinerezibusiness@gmail.com>
2025-09-30 03:08:38 +0700

    Update takehometestds_chatharina_cheisha.py

--- app/takehometestds_chatharina_cheisha.py
+++ app/takehometestds_chatharina_cheisha.py
@@ -15,7 +15,6 @@ Membangun model prediktif untuk memperkirakan Delivery_Time_min (menit) per pesa
 import os, datetime as dt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
 import altair as alt
 
